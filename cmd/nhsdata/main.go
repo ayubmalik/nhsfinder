1bc0c8f2a868c8ac3281cb69357e2cdabcd8ca1a
Ayub Malik <ayub.malik@coopdigital.co.uk>
2019-09-02 17:32:43 +0100

    Add nhsdata cmd to download and create required pharamcy.csv

--- cmd/nhsdata/main.go
+++ cmd/nhsdata/main.go
@@ -8,13 +8,15 @@ import (
 	"path"
 	"strings"
 
+	"github.com/ayubmalik/nhsfinder"
+
 	"github.com/ayubmalik/nhsfinder/data"
 	"github.com/mholt/archiver"
 )
 
 const (
-	dispensariesURL = "https://files.digital.nhs.uk/assets/ods/current/edispensary.zip"
-	headquartersURL = "https://files.digital.nhs.uk/assets/ods/current/epharmacyhq.zip"
+	dispensariesURL    = "https://files.digital.nhs.uk/assets/ods/current/edispensary.zip"
+	postcodesLatLngURL = "https://www.freemaptools.com/download/full-postcodes/ukpostcodes.zip"
 )
 
 func main() {
@@ -22,18 +24,27 @@ func main() {
 	defer func() {
 		os.RemoveAll(tmp)
 	}()
-	fmt.Println(tmp)
+	fmt.Println("tmp dir", tmp)
 
-	dispZip := fetchURL(tmp, dispensariesURL)
+	fmt.Println("fetching", dispensariesURL)
+	dispZip := fetchURL(dispensariesURL, tmp)
 	archiver.Unarchive(dispZip, tmp)
 
+	fmt.Println("fetching", postcodesLatLngURL)
+	pcodesZip := fetchURL(postcodesLatLngURL, tmp)
+	archiver.Unarchive(pcodesZip, tmp)
+
+	pcodesCsv := strings.TrimSuffix(pcodesZip, ".zip") + ".csv"
+	pcodes := nhsfinder.LoadPostcodes(pcodesCsv)
+
+	fmt.Println("create pharmacies")
 	dispCsv := strings.TrimSuffix(dispZip, ".zip") + ".csv"
-	data.CreatePharmacies(dispCsv, nil, "/tmp/pharmacies.csv")
+	data.CreatePharmacies(dispCsv, pcodes, "/tmp/pharmacies.csv")
 }
 
-func fetchURL(tmp string, url string) string {
+func fetchURL(url string, dir string) string {
 	fetcher := data.HTTPFetcher{}
-	dest := path.Join(tmp, path.Base(url))
+	dest := path.Join(dir, path.Base(url))
 	err := fetcher.Fetch(url, dest)
 	if err != nil {
 		log.Fatalf("could not download %s!\n", url)
--- loadpostcodes.go
+++ loadpostcodes.go
@@ -9,11 +9,11 @@ import (
 )
 
 // LoadPostcodes loads Postcode struct from CSV filename as map keyed off postcode string
-func LoadPostcodes(filename string) map[string]Postcode {
+func LoadPostcodes(filename string) map[string]LatLng {
 	datafile, _ := os.Open(filename)
 	defer datafile.Close()
 	r := csv.NewReader(datafile)
-	var postcodes = make(map[string]Postcode)
+	var postcodes = make(map[string]LatLng)
 	r.Read() // skip header
 	for {
 		record, err := r.Read()
@@ -28,12 +28,9 @@ func LoadPostcodes(filename string) map[string]Postcode {
 		value := record[1]
 		lat, _ := strconv.ParseFloat(record[2], 64)
 		lng, _ := strconv.ParseFloat(record[3], 64)
-		postcodes[value] = Postcode{
-			Value: value,
-			LatLng: LatLng{
-				Lat: lat,
-				Lng: lng,
-			},
+		postcodes[value] = LatLng{
+			Lat: lat,
+			Lng: lng,
 		}
 	}
 	return postcodes
