834869f6cdb9753e8969b99c8f1301e8bf4403f4
Ayub Malik <ayub.malik@gmail.com>
2019-09-04 20:24:09 +0100

    Add jwt util wip

---
+++ cmd/jwt/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+	parts := strings.Split(token, ".")
+	payload, _ := base64.RawURLEncoding.DecodeString(parts[1])
+	fmt.Println(string(payload))
+}
