96824b2ab723951f55535b65d0756032b3fb3dd2
Ayub Malik <ayub.malik@gmail.com>
2019-10-09 21:17:15 +0100

    Fix build ok 3

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,40 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
 
 	finder "github.com/ayubmalik/pharmacyfinder"
-
-	"goji.io/pat"
 )
 
-type finderRoute struct {
-	finder finder.Finder
-}
-
-func (fr finderRoute) serveHTTP(w http.ResponseWriter, r *http.Request) {
-	postcode := strings.ToUpper(pat.Param(r, "postcode"))
-	postcode = strings.Replace(postcode, "+", " ", -1) // allow M4+4BF
-	result := fr.finder.ByPostcode(postcode)
-	jsonOut, _ := json.Marshal(result)
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	fmt.Fprintf(w, string(jsonOut))
-}
-
-func search(w http.ResponseWriter, r *http.Request) {
-	content, err := ioutil.ReadFile("search.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Fprintf(w, string(content))
-}
-
 func main() {
+	// TODO
+	// w.Header().Set("Access-Control-Allow-Origin", "*")
+	// postcode = strings.Replace(postcode, "+", " ", -1) // allow M4+4BF
 	log.Println("Loading data from CSV files")
 	latLngs := finder.LoadLatLngs("data/ukpostcodes.csv")
 	pharmacies := finder.LoadPharmacies("data/Pharmacy.csv")
