4345304fa979cc3766f3b7008d090ddb0167d7d5
Ayub Malik <ayub.malik@coopdigital.co.uk>
2020-01-27 16:54:47 +0000

    Refactor out data subpackage code

---
+++ pharmacies.go
@@ -0,0 +1,68 @@
+package pharmacyfinder
+
+import (
+	"encoding/csv"
+
+	"fmt"
+	"golang.org/x/text/encoding/charmap"
+	"os"
+	"strings"
+)
+
+// SimplifyPharmacies takes NHS ODS Pharmacy.CSV file and creates a simplified CSV.
+// The resulting pharmacy CSV file contains no header and only the following fields:
+//	 ODSCode, Name, Address1, Address2, Address3, Address4, Postcode, Telephone, Email, Lat, Lng
+//
+// For source data see:
+// 	http://media.nhschoices.nhs.uk/data/foi/Pharmacy.csv
+func SimplifyPharmacies(pharmacyCSV string, outputCsv string) error {
+	f, err := os.Open(pharmacyCSV)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	iso88591 := charmap.ISO8859_1.NewDecoder().Reader(f)
+	reader := csv.NewReader(iso88591)
+	reader.Comma = '¬'
+	reader.TrimLeadingSpace = true
+	reader.Read() // skip first
+	rows, _ := reader.ReadAll()
+
+	fmt.Println(len(rows))
+	pharmacies := []string{}
+	for _, row := range rows {
+		pcode := row[13]
+		lat := row[14]
+		lng := row[15]
+		p := fmt.Sprintf("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s",
+			row[1], clean(row[7]), clean(row[8]), clean(row[9]), clean(row[10]), clean(row[11]), pcode, row[18], row[19], lat, lng)
+		pharmacies = append(pharmacies, p)
+	}
+	return write(outputCsv, pharmacies)
+}
+
+func clean(src string) string {
+	return strings.Replace(strings.Trim(src, " "), ",", "", -1)
+}
+
+func appendLatLon(pharmacyRows []string, postcodes map[string]LatLng) {
+	fmt.Println(postcodes)
+}
+
+func write(file string, pharmacies []string) error {
+	f, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	last := len(pharmacies) - 1
+	for i, v := range pharmacies {
+		nl := "\n"
+		if i == last {
+			nl = ""
+		}
+		fmt.Fprintf(f, "%s%s", v, nl)
+	}
+	return nil
+}
---
+++ pharmacies_test.go
@@ -0,0 +1,31 @@
+package pharmacyfinder
+
+import (
+	"bytes"
+	"fmt"
+
+	"io/ioutil"
+	"testing"
+)
+
+func TestSimplifyPharmacies(t *testing.T) {
+
+	t.Run("Simplify pharmacies", func(t *testing.T) {
+		inputFile := "testdata/Pharmacy.csv"
+		goldenFile := "testdata/pharmacies.golden.csv"
+		outputFile := "/tmp/pharmacies.csv"
+
+		if err := SimplifyPharmacies(inputFile, outputFile); err != nil {
+			t.Fatalf("%v", err)
+		}
+
+		expected, _ := ioutil.ReadFile(goldenFile)
+		actual, _ := ioutil.ReadFile(outputFile)
+
+		fmt.Println(string(actual))
+
+		if !bytes.Equal(expected, actual) {
+			t.Fatalf("actual/expected contents not equal:\n%s\n\n%s", actual, expected)
+		}
+	})
+}
